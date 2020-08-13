48c22a4a0d32c189c0e2cec0af1b0f7a59848c57
Maxence Revéret <mxreveret@gmail.com>
2020-08-13 16:59:40 +0200

    version plus evolutive

--- index.js
+++ index.js
@@ -1,4 +1,78 @@
 /**
+ * Creer une instance de TypeSport
+ *
+ * @constructor
+ * @this {TypeSport}
+ * @param {string} sport Le type de sport
+ * @param {number} gestion1 une regle de gestion , ici la vitesse max autorisee par exemple
+ */
+class TypeSport {
+	constructor (sport,gestion1) {
+		this.sport = sport;
+		this.gestion1  =gestion1;
+	}
+}
+/**
+ * Creer une instance de TypeSport
+ *
+ * @constructor
+ * @this {Walking}
+ * @param {number} gestion1 une regle de gestion , ici la vitesse max autorisee par exemple
+ */
+
+class Walking extends TypeSport{
+	constructor(gestion1){
+		super('walking',116.7);
+		if (gestion1 !== undefined)
+			this.gestion1 = gestion1;
+	}
+}
+/**
+ * Creer une instance de Running
+ *
+ * @constructor
+ * @this {Running}
+ * @param {number} gestion1 une regle de gestion , ici la vitesse max autorisee par exemple
+ */
+
+class Running extends TypeSport {
+	constructor(gestion1){
+		super('running',300);
+		if (gestion1 !== undefined)
+			this.gestion1 = gestion1;
+	}
+}
+/**
+ * Creer une instance de TypeSport
+ *
+ * @constructor
+ * @this {Cycling}
+ * @param {number} gestion1 une regle de gestion , ici la vitesse max autorisee par exemple
+ */
+
+class Cycling extends TypeSport {
+	constructor(gestion1){
+		super(Cycling, 833,5);
+		if (gestion1 !== undefined)
+			this.gestion1 = gestion1;
+	}
+}
+/**
+ * Creer une instance de NewSport (n'importe quel nouveau sport)
+ *
+ * @constructor
+ * @this {NewSport}
+ * @param {number} gestion1 une regle de gestion , ici la vitesse max autorisee par exemple
+ */
+
+class NewSport extends TypeSport{
+	constructor(sport,gestion1) {
+		super(sport,gestion1);
+	}
+}
+
+var sport = ['walking','running','cycling']; // Array representant les types de sport existant
+/**
  * Cree une instance de Activity
  * 
  * @constructor
@@ -10,13 +84,30 @@
  */
 class Activity {
 	static getSportTypes() {
-		return 'Type can only be : running, cycling, walking\n';
+		let ret = 'Type can only be :';
+		let i = 0;
+		while (i < sport.length)
+		{
+			if (i === 0)
+			ret = ret.concat(' ',sport[i]);
+			else
+			ret = ret.concat(', ',sport[i]);
+			i++;
+		}
+		ret.concat('','\n');
+		return ret;
 	}
-
-	constructor(type, distance, duration) {
+	constructor(type, distance, duration,gestion1) {
 		if (!([type,distance,duration].every(arg => arg !== undefined)))
 			throw new Error ('Missing argument\n');
-		this.type = type;
+		if (type  === 'running')
+			this.type = new Running(gestion1);
+		else if (type  === 'walking')
+			this.type = new Walking(gestion1);
+		else if (type  === 'cycling')
+			this.type = new Cycling(gestion1);
+		else
+			this.type = new NewSport(type, gestion1);
 		this.distance = distance;
 		this.duration = duration; 
 	}
@@ -28,38 +119,15 @@ class Activity {
  * @this {Marathon}
  * @param {number} duration La duree de l'activite en min
  * */
+
 class Marathon extends Activity
 {
 	constructor(duration)
 	{
-		super('running',42195,duration);
-		this.duration = duration;
-		this.max_speed = 250;
+		super('running',42195,duration,250);
 	}
 };
 
-/**
- * Defini la vitesse maximale autorisee (en metres/minutes)
- *
- * @param {Activity} obj Toute instance d'Activity ou d'instance d'objet heritant d'Activity (comme Marathon)
- * @return {number} max_speed vitesse maximale autorisee
- */
-function defineMaxSpeed(obj)
-{
-	let max_speed = 0;
-	if (obj.max_speed)
-		return obj.max_speed
-	else
-	{
-		if (obj.type === 'running')
-			max_speed = 300;
-		if (obj.type === 'cycling')
-			max_speed = 833,4;
-		if (obj.type === 'walking')
-			max_speed = 116,7;
-	}
-	return max_speed;
-}
 /**
  * Teste la validitee des attribut d'une instance d'Activity (ou instance de classes heritant d'Activity)
  *
@@ -68,20 +136,26 @@ function defineMaxSpeed(obj)
  */
 function isValid (obj)
 {
-	if (obj.type != 'running' && obj.type != 'cycling' && obj.type != 'walking')
+	if (!(sport.includes(obj.type.sport)))
 		return Activity.getSportTypes();
 	if (obj.distance < 0)
 		return 'Invalid distance\n';
 	if (obj.duration < 0)
 		return 'Invalid duration\n';
 	const speed = obj.distance / obj.duration;
-	const  max_speed = defineMaxSpeed(obj);
-	if (speed > max_speed)
+	if (speed > obj.type.gestion1)
 		return 'Speed is invalid\n';
 	return 'Everything is valid\n';
 }
 
-module.exports.defineMaxSpeed = defineMaxSpeed;
+
+
 module.exports.isValid = isValid;
 module.exports.Activity = Activity;
 module.exports.Marathon = Marathon;
+module.exports.TypeSport = TypeSport;
+module.exports.Running = Running;
+module.exports.Walking = Walking;
+module.exports.Cycling = Cycling;
+module.exports.NewSport = NewSport;
+module.exports.sport = sport;
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const {isValid,defineMaxSpeed, Activity, Marathon} = require('./index.js');
+const {isValid, Activity, Marathon, TypeSport, Running, Cycling, Walking, NewSport, sport} = require('./index.js');
 
 let test = new Activity('running',1000,60);
 let test3 = new Activity('ring',1000,60);
@@ -6,6 +6,8 @@ let test2 = new Marathon(1,1,240);
 let test4 = new Activity('running',-1000,60);
 let test5 = new Activity('running',1000,-60);
 let test6 = new Marathon(10);
+
+
 
 QUnit.test('Wrong value', function (assert){
 		assert.equal(isValid(test2),'Speed is invalid\n', 'Speed too high\n');
@@ -15,6 +17,9 @@ QUnit.test('Wrong value', function (assert){
 		assert.equal(isValid(test6),'Speed is invalid\n', 'Speed too high for a Marathon\n');
 		});
 
+let test7 = new Activity('swimming',1000,2,100000)
+	sport.push('swimming');
 QUnit.test('Good values', function (assert){
 		assert.equal(isValid(test),'Everything is valid\n', 'Everything is ok\n');
+		assert.equal(isValid(test7),'Everything is valid\n', 'Everything is ok with new sport\n');
 		});
